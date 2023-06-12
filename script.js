de31a1ae0f79d844edd2fe26a83c203b8c65c33d
bianca9901 <biancawikberg99@yahoo.com>
2023-06-12 08:18:15 +0200

    Add css file

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const startButton = document.getElementById('start-button')
 const nextButton = document.getElementById('next-button')
 const quizBoxElement = document.getElementById ('quiz-box')
 const questionElement = document.getElementById('question')
+const scoreElement = document.getElementById('score')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
-let rearrangeQuestions, currentQuestionIndex
+
+let rearrangeQuestions
+let currentQuestionIndex = 0
+let score = 0
+console.log(nextButton)
 
 startButton.addEventListener('click', startQuiz)
 nextButton.addEventListener('click',() => {
@@ -12,19 +17,34 @@ nextButton.addEventListener('click',() => {
     setNextQuestion()
 })
 function startQuiz() {
+    // start the quiz, hide the start button
+  // show the question box
+  //  show the next button
     startButton.classList.add('hide')
+    nextButton.classList.remove('hide')
     rearrangeQuestions = questions.sort(() => Math.random - .5)
     currentQuestionIndex = 0
+    score = 0
     quizBoxElement.classList.remove('hide')
     setNextQuestion()
 }
 
 function setNextQuestion() {
-    resetState()
-    showQuestion(rearrangeQuestions [currentQuestionIndex])
-
-}
-
+        resetState()
+        nextButton.classList.remove('hide')
+        console.log(rearrangeQuestions.length)
+        console.log(currentQuestionIndex + 1)
+        numberOfQuestion = currentQuestionIndex + 1
+        console.log(`numberOfQuestion ${numberOfQuestion}`)
+        if (rearrangeQuestions.length >= numberOfQuestion) {
+          showQuestion(rearrangeQuestions [currentQuestionIndex])
+        } else {
+          //  stop the quiz
+          nextButton.classList.add('hide')
+          
+        }
+    }
+    
 function showQuestion(question) {
     questionElement.innerText = question.question
     question.answer.forEach(answer => {
@@ -79,23 +99,82 @@ function clearStatusClass(element) {
 
 const questions = [
     {
-        question: 'que es',
-        answer : [
-            { text: 'uno', correct: true },
-            { text: 'dos', correct: false },
-            { text: 'tres', correct: false },
-            { text: 'cuatro', correct: false }
-
+        question: 'Cheers in Spanish',
+        answer: [
+            { text: 'Skål', correct: false},
+            { text: 'Cheers', correct: false },
+            { text: 'Salud', correct: true},
+            { text: 'Prost', correct: false },
         ]
     },
 
 {
-    question: 'que es',
+    question: 'Cheers in French',
         answer: [
-            { text: 'hola', correct: true },
-            { text: 'helo', correct: false },
-            { text: 'hi', correct: false },
-            { text: 'hej', correct: false }
+            { text: 'Sláinte', correct: false },
+            { text: 'Şerefe', correct: false },
+            { text: 'Kanpai', correct: false },
+            { text: 'Santé', correct: true },
+        ]
+},
+{
+    question: 'Cheers in Afrikaans',
+        answer: [
+            { text: 'Gesondheid', correct: true },
+            { text: 'Yamas', correct: false },
+            { text: 'Gānbēi', correct: false },
+            { text: 'Prost', correct: false }
         ]
 }
 ]
+
+/*Add after fix quiz bug
+question: 'Cheers in German',
+        answer: [
+            { text: 'Skål', correct: false},
+            { text: 'Geonbae', correct: false },
+            { text: 'Salud', correct: false },
+            { text: 'Prost, correct: true }
+
+question: Cheers in Swedish,
+        answer: [
+            { text: 'Skål', correct: true },
+            { text: 'Santé', correct: false },
+            { text: 'Ziveli', correct: false },
+            { text: 'Prost', correct: false }
+
+question: Cheers in Italian,
+        answer: [
+            { text: 'Skål', correct: false },
+            { text: 'Cheers', correct: false },
+            { text: 'Geonbae', correct: false },
+            { text: 'Cin Cin', correct: true }
+
+question: Cheers in Japanese,
+        answer: [
+            { text: 'Kanpai', correct: true },
+            { text: 'Gānbēi', correct: false },
+            { text: 'Gom bui', correct: false },
+            { text: 'Prost', correct: false }
+
+question: Cheers in Chinese (Cantonese),
+        answer: [
+            { text: 'Kanpai', correct: false },
+            { text: 'Gānbēi', correct: false },
+            { text: 'Gom bui', correct: true },
+            { text: 'Prost', correct: false }
+
+question: Cheers in Serbian,
+        answer: [
+            { text: 'Ziveli', correct: true },
+            { text: 'Gānbēi', correct: false },
+            { text: 'Gom bui', correct: false },
+            { text: 'Prost', correct: false }
+
+question: Cheers in Albanian,
+        answer: [
+            { text: 'Kanpai', correct: false },
+            { text: 'Gëzuar', correct: true },
+            { text: 'Gom bui', correct: false },
+            { text: 'Prost', correct: false }
+*/
